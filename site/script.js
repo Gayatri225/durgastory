9a50a902074902c23db8c561f727a4aa5f6ea043
Gayatri <n.gayatri22@gmail.com>
2025-08-31 22:10:03 +0530

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,9 +1,3 @@
-function placeholder(img, name){
-  const d=document.createElement('div');
-  d.className='placeholder';
-  d.innerHTML = `Add <code>${name}</code> to <code>site/images/</code>`;
-  img.replaceWith(d);
-}
 const btn = document.getElementById('toggleTheme');
 if (btn){
   btn.addEventListener('click', ()=>{
